955265ecd4848a177ba39e4fa08aeda18f0128cc
Fayed Gaya <gayafayed@gmail.com>
2022-12-09 03:49:57 -0500

    Add capability to compute training face matrix

--- main.py
+++ main.py
@@ -1,31 +1,81 @@
-# Implement Eigneface
-# Please see project description below. Due date: Tues 12/20 @ 11:00 pm. Deadline for late Project 2 (with penalty): Fri 12/30 @ 11:00 pm. The Face dataset for training and testing your program is attached below. Face dataset: The Face Dataset consists of the Training Set and the Testing Set. The images are in .jpg format and are of dimension 195 x 231 (width x height) pixels. Each pixel uses 8 bits for grayscale. The Training Set consists of eight images from eight different persons. Use all eight training images in the Training Set to train for the Eigenfaces and put all eight Eigenfaces in the matrix U. The Testing Set consists of six images from four different persons. Recognize the faces in the Testing Set using the Eigenface method.  Note: In the lecture slides, the training and test images are of size N X N (same horizontal and vertical dimensions.) The face images in the above dataset have different horizontal and vertical dimensions, but the Eigenface method works the same way with no changes to the formulas. 
+# Used for matrix operations
+import numpy as np
 
-# Load in training images
-# Apply Priciple Componenet Analysis to training face images
-# Project face image onto the face space and represent using PCA coefficients
-# Using PCA coefficients as weights and reconstruct the origianl face image using a linear combination of eigenfaces (eigenvectors)
-# Match testing facs by computing the distance between the PCA coefficents of the testing face with the training faces
+# Used for loading images
+import cv2 as cv
 
+# Used to navigate directories
+import os
 
-# More technically:
-# For each training image i of size X x Y pixels, create rowstacked column vectors (Ri) of dimension X x Y for M training images
-# Compute mean face (m) by taking the average of the M training face images
-# Subtract the mean face m from each training face, ->Ri = Ri - m
-# Put all training faces into a single matrix A (dimension N^2 x M): [R1, r2, r3, ..., Rm]
-# ...
+# Training image set path
+TRAINING_FACE_SET = "Face dataset/Training/"
+# Training image width
+IMAGE_WIDTH = 195
+# training image height
+IMAGE_HEIGHT = 231
+# Number of Training Images
+N_TRAINING_IMAGES = 8
+
 
 def main():
-    pass
+    """
+    Implement the Eigenface method for face recognition.
+    """
+
+    # Create training face matrix
+    A = calculuate_training_face_matrix(TRAINING_FACE_SET)
+
+    # Debugging
+    print(A.shape)
 
 
-def load_training_data():
-    pass
+def calculuate_training_face_matrix(directory):
+    """
+    Return a 2D numpy matrix made up of normalized column vector representations of the training images
+    : param directory: The path of the directory containing the training images
+    """
+
+    # Store column vectors
+    column_vectors = []
+    # Iterate through training images
+    for filename in os.listdir(directory):
+        # Create image object
+        img = cv.imread(directory + filename, 0)
+        # store image shape
+        rows, cols = img.shape
+
+        # Store image pixel values
+        pixel_values = []
+        for i in range(rows):
+            for j in range(cols):
+                pixel = img[i, j]
+                pixel_values.append(pixel)
+
+        # Add image column vector to unprocessed column vector matrix
+        column_vectors.append(pixel_values)
+
+    # Create mean face
+    m = []
+    # Initlaize mean face
+    for i in range(IMAGE_WIDTH * IMAGE_HEIGHT):
+        m.append(0)
+
+    # Sum column vectors to get meanface value before averaging
+    for i in range(N_TRAINING_IMAGES):
+        for j in range(IMAGE_WIDTH * IMAGE_HEIGHT):
+            m[j] += column_vectors[i][j]
+
+    # Average the values of the meanface vector to get the final meanface
+    for i in range(len(m)):
+        m[i] = int(m[i] / N_TRAINING_IMAGES)
+
+    # Subtract the meanface column vector from each training face
+    for i in range(N_TRAINING_IMAGES):
+        for j in range(IMAGE_WIDTH * IMAGE_HEIGHT):
+            column_vectors[i][j] -= m[j]
+
+    return np.array(column_vectors)
 
 
-def compute_mean_face():
-    pass
-
-
-def modify_training_faces(mean_face):
-    pass
+if __name__ == "__main__":
+    main()
